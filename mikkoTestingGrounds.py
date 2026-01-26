f68f7895b31f5393f7414a85205ad43f369786ee
Mikko-Dakota Roberts <109138693+MikkoRoberts@users.noreply.github.com>
2026-01-26 14:32:48 -0800

    Update mikkoTestingGrounds.py

--- mikkoTestingGrounds.py
+++ mikkoTestingGrounds.py
@@ -1,8 +1,21 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-x = np.linspace(1,100,100)
-print(x)
+t = np.linspace(1,100,100)  # sec
+dt = 1 # sec
 
-for x in x:
-    y = x + 2
+x = 0 # m
+y = 0 # m 
+
+vX = 3 # m/s
+vY = 20 # m/s
+
+aY = -9.81 # m/s^2
+aX = 0 # m/s^2
+
+for t in t:
+
+    y = y + vY*dt + 0.5*aY*(dt^2)
+    vY = vY + aY*dt
     print(y)
+    print(vY)
