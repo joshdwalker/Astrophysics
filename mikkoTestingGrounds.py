8edaf8b25a9d45abd46c08e7c24f2340996230c9
Mikko Roberts <strikingrock633@gmail.com>
2026-01-26 16:18:12 -0800

    Refactor simulation code in mikkoTestingGrounds.py to include physics calculations for position, velocity, acceleration, and jerk

--- mikkoTestingGrounds.py
+++ mikkoTestingGrounds.py
@@ -1,10 +1,35 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-x = np.linspace(1,100,100)
-print(x)
 
-for x in x:
-    y = x + 2
-    print(y)
- 
- #rrrrr
+
+t = range(0,100)
+dt = 1
+
+x = 0
+y = 20
+
+vX = 3
+vY = 20
+
+aX = 0
+aY = -9.81
+
+jX = 0
+jY = 0
+
+for i in t:
+
+    aX = aX + jX*dt
+    aY = aY + jY*dt
+
+    vX = vX + aX*dt
+    vY = vY + aY*dt
+
+    x = x + vX*dt
+    y = y + vY*dt
+
+    print("Position:", [x, y])
+    print("Velocity:", [vX, vY])
+    print("Acceleration:", [aX, aY])
+    print("Jerk:", [jX, jY])
