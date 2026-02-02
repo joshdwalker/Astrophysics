feb68cb01054f19dc5e578e2ce5a3a4e8064771c
Mikko Roberts <strikingrock633@gmail.com>
2026-02-02 15:06:37 -0800

    bs

--- mikkoTestingGrounds.py
+++ mikkoTestingGrounds.py
@@ -1,22 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-
 dt = 0.01
 t = np.linspace(0, 100, int(10 / dt))
 
 x = 0
-y = 20
-z = 0
+y = 0
+z = 20 
 
 vX = 3
-vY = 20
-vZ = 0
+vY = 0
+vZ = 20
 
 aX = 0
-aY = -9.81
-aZ = 0
+aY = 0
+aZ = -9.81
 
 jX = 0
 jY = 0
@@ -37,17 +35,17 @@ jZs = []
 
 for _ in t:
 
-    aX = aX + jX*dt
-    aY = aY + jY*dt
-    aZ = aZ + jZ*dt
+    aX = aX + jX * dt
+    aY = aY + jY * dt
+    aZ = aZ + jZ * dt
 
-    vX = vX + aX*dt
-    vY = vY + aY*dt
-    vZ = vZ + aZ*dt
+    vX = vX + aX * dt
+    vY = vY + aY * dt
+    vZ = vZ + aZ * dt
 
-    x = x + vX*dt
-    y = y + vY*dt
-    z = z + vZ*dt
+    x = x + vX * dt
+    y = y + vY * dt
+    z = z + vZ * dt
 
     xs.append(x)
     ys.append(y)
@@ -65,19 +63,11 @@ for _ in t:
     jYs.append(jY)
     jZs.append(jZ)
 
-    if y <= 0:
+    if z <= 0:
         break
 
-   
-print("Position:", [xs, ys, zs])
-print("new line")
-print("Velocity:", [vXs, vYs, vZs])
-print("new line")
-print("Acceleration:", [aXs, aYs, aZs])
-print("new line")
-print("Jerk:", [jXs, jYs, jZs])
-print("new line")
-
+print("Final position:", (xs[-1], ys[-1], zs[-1]))
+print("Final velocity:", (vXs[-1], vYs[-1], vZs[-1]))
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
