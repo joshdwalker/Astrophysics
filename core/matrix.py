02b06f2461be5502f188b47e7de8a28d0a7e5250
Joshua <jdwalker4598@gmail.com>
2026-02-16 16:50:52 -0500

    fixed code standard on matrix.py

--- core/matrix.py
+++ core/matrix.py
@@ -1,55 +1,55 @@
-import numpy
+import numpy as np
 from math import sin, cos, tan, pi
 
 class Matrix(object):
 
     @staticmethod
     def make_identity():
-        return numpy.array([[1, 0, 0, 0],
-                            [0, 1, 0, 0],
-                            [0, 0, 1, 0],
-                            [0, 0, 0, 1]]).astype(float)
+        return np.array([[1, 0, 0, 0],
+                         [0, 1, 0, 0],
+                         [0, 0, 1, 0],
+                         [0, 0, 0, 1]]).astype(float)
     
     @staticmethod
     def make_translation(x, y, z):
-        return numpy.array([[1, 0, 0, x],
-                            [0, 1, 0, y],
-                            [0, 0, 1, z],
-                            [0, 0, 0, 1]]).astype(float)
+        return np.array([[1, 0, 0, x],
+                         [0, 1, 0, y],
+                         [0, 0, 1, z],
+                         [0, 0, 0, 1]]).astype(float)
     
     @staticmethod
     def make_rotation_x(angle):
         c = cos(angle)
         s = sin(angle)
-        return numpy.array([[1, 0, 0, 0],
-                            [0, c, -s, 0],
-                            [0, s, c, 0],
-                            [0, 0, 0, 1]]).astype(float)
+        return np.array([[1, 0, 0, 0],
+                         [0, c, -s, 0],
+                         [0, s, c, 0],
+                         [0, 0, 0, 1]]).astype(float)
     
     @staticmethod
     def make_rotation_y(angle):
         c = cos(angle)
         s = sin(angle)
-        return numpy.array([[c, 0, s, 0],
-                            [0, 1, 0, 0],
-                            [-s, 0, c, 0],
-                            [0, 0, 0, 1]]).astype(float)
+        return np.array([[c, 0, s, 0],
+                         [0, 1, 0, 0],
+                         [-s, 0, c, 0],
+                         [0, 0, 0, 1]]).astype(float)
     
     @staticmethod
     def make_rotation_z(angle):
         c = cos(angle)
         s = sin(angle)
-        return numpy.array([[c, -s, 0, 0],
-                            [s, c, 0, 0],
-                            [0, 0, 1, 0],
-                            [0, 0, 0, 1]]).astype(float)
+        return np.array([[c, -s, 0, 0],
+                         [s, c, 0, 0],
+                         [0, 0, 1, 0],
+                         [0, 0, 0, 1]]).astype(float)
 
     @staticmethod
     def make_scale(s):
-        return numpy.array([[s, 0, 0, 0],
-                            [0, s, 0, 0],
-                            [0, 0, s, 0],
-                            [0, 0, 0, 1]]).astype(float)
+        return np.array([[s, 0, 0, 0],
+                         [0, s, 0, 0],
+                         [0, 0, s, 0],
+                         [0, 0, 0, 1]]).astype(float)
 
     @staticmethod
     def make_perspective(angle_of_view = 60, aspect_ratio = 1, near = 0.1, far = 1000):
@@ -58,7 +58,7 @@ class Matrix(object):
         r = aspect_ratio
         b = (near + far) / (near - far)
         c = 2 * near * far / (near - far)
-        return numpy.array([[d / r, 0, 0, 0],
-                            [0, d, 0, 0],
-                            [0, 0, b, c],
-                            [0, 0, -1, 0]]).astype(float)
+        return np.array([[d / r, 0, 0, 0],
+                         [0, d, 0, 0],
+                         [0, 0, b, c],
+                         [0, 0, -1, 0]]).astype(float)
