21c8a170fd4945f6f975c6b3bb6f9c6ba52ae5b3
Joshua <jdwalker4598@gmail.com>
2026-03-08 19:53:39 -0400

    Added apply_matrix() and merge() methods to Geometry to allow composite surfaces like cylinders, and to allow transformations of surfaces (translations primarily)

--- core/geometry/geometry.py
+++ core/geometry/geometry.py
@@ -1,4 +1,5 @@
 from core.attribute import Attribute
+import numpy as np
 
 class Geometry(object):
     def __init__(self):
@@ -16,3 +17,35 @@ class Geometry(object):
         #number of vertices may be calculated from the length of any Attribute object's array of data
         attribute = list(self.attributes.values())[0]
         self.vertex_count = len(attribute.data)
+
+    #transform the data in an attribute using a matrix
+    def apply_matrix(self, matrix, variable_name = 'vertex_position'):
+        old_position_data = self.attributes[variable_name].data
+        new_position_data = []
+        
+        for old_position in old_position_data:
+            #avoid changing list references
+            new_position = old_position.copy()
+            #add homogeneous fourth coordinate
+            new_position.append(1)
+            #multiply by matrix
+            new_position = np.matmul(matrix, new_position)
+            #remove homogeneous coordinate
+            new_position = list(new_position[0:3])
+            #add to new data list
+            new_position_data.append(new_position)
+        
+        self.attributes[variable_name].data = new_position_data
+        #new data must be uploaded
+        self.attributes[variable_name].upload_data()
+    
+    #merge data from attributes of other geometry into this object
+    #requires both geometries to have attributes with the same names
+    def merge(self, other_geometry):
+        for variable_name, attribute_object in self.attributes.items():
+            attribute_object.data += other_geometry.attributes[variable_name].data
+            #new data must be uploaded
+            attribute_object.upload_data()
+        
+        #update the number of vertices
+        self.count_vertices()
